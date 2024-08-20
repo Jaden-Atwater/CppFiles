26c766cdbbe2e03ee587e1958e2bf016e6933973
Jaden-Atwater <138690210+Jaden-Atwater@users.noreply.github.com>
2024-08-20 17:04:20 -0400

    JatwaterLab1Q1.cpp

--- JatwaterLab1Q1.cpp
+++ JatwaterLab1Q1.cpp
@@ -2,15 +2,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int a;
+int main() 
+{
+ 	string name; //string will be used for any variable with letters
 	cout << "Enter your full name: "; //asking for you to type in your name then hit enter
-	cin >> a; //saving input from user 
-	
-	cout << "Hi, your name is: " << a; //returning input from user
-	
+	getline(cin, name); //reads the whole line
 	
  
+	int studentID; //need a int because of numbers
+	cout << "Enter your 9 digit student ID: "; 
+	cin >> studentID;  
+	
+
+	string classification;
+	cout << "Enter your institution level: ";  
+	getline(cin, classification);   
+	
+	 
+	string major;
+	cout << "Enter your major: ";  
+	getline(cin, major);  
+	
+
+ 	cout << "Hi, your name is: " << endl;  
+ 	cout << "You're GSU student ID is: " << endl; 
+	cout << "You're currently a: " << endl;  
+	cout << "You're studying: " << endl;  
+	
+	return 0;
+	
 }
 
  
